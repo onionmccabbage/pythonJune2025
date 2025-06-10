69d1e38aeba3de5dd297d49509bba4bcb53ac35e
onionmccabbage <onionmccabbage@live.com>
2025-06-10 13:30:06 +0100

    text for review 1

--- structure.py
+++ structure.py
@@ -4,7 +4,7 @@ import random # random is one of many modules in the Python Standard library
 r = random.randint(0,10) # a random number between 0 and 10 inclusive
 
 # we might want a never-ending routine
-while True:
+while True: # this is often called a 'run-loop' - where we need to run endlessly
     '''the while operator lets us specify a condition for the loop'''
     g = input('Guess the number: ')
     g_int = int(float(g))
