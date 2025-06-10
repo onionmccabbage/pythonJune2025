43b55bb63ac2716e05417cca70d4412135f91731
onionmccabbage <onionmccabbage@live.com>
2025-06-10 13:16:56 +0100

    sinple guessing game

---
+++ structure.py
@@ -0,0 +1,16 @@
+# we may choose to make use of other modules
+import random # random is one of many modules in the Python Standard library
+
+r = random.randint(0,10) # a random number between 0 and 10 inclusive
+
+# we might want a never-ending routine
+while True:
+    '''the while operator lets us specify a condition for the loop'''
+    g = input('Guess the number: ')
+    g_int = int(float(g))
+    if g_int == r:
+        print(f'You guessed {r} corretly')
+        break # this will break out of our endless loop
+    else:
+        print('Nope...')
+        # continue
