81f4d388a76b93d0f6778a5a4f66ccb79007eb69
onionmccabbage <onionmccabbage@live.com>
2025-06-10 11:54:54 +0100

    using if logic operators

--- logic.py
+++ logic.py
@@ -13,8 +13,29 @@ print(n, type(n))
 # %  remainder division
 p = n**2 # raie eto power 2
 d = n//2 # modulo division
-r = n%2  # remainder after division
-print(p,d, r)
+r=n%2  # remainder after division
+print(p, d, r)
 # we may use == to compare values
 if n == 2:
     print('you entered two')
+elif n>10:
+    print(f'{n} is bigger than ten')
+elif n<0:
+    print(f'{n} is a negative number')
+else:
+    print('Some other value was entered')
+
+# using logical operators including and or in
+primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+if n in primes:
+    print(f'{n} is a prime number between 0 and 100')
+
+# using and/or
+# challenge is it both an odd number AND a prime number
+if n//2 != n/2 and n in primes:
+    print(f'{n} is an odd prime number')
+
+# we also have <= and >=
+# we may use or...
+if n==5 or n==-5:
+    print('FIVE!!!')
