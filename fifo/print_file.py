e76f96ef7d4d6a53fb8c2ee146763060e1d41742
onionmccabbage <onionmccabbage@live.com>
2025-06-11 15:32:52 +0100

    print to file

--- fifo/print_file.py
+++ fifo/print_file.py
@@ -4,7 +4,9 @@ def printOutput(s):
     '''Persist the incoming sting of text into a text file
     we will append to file with each new piece of text'''
     # we need a file access object
+    # NB we may use a relative path or an absolute path
     fout = open('my_log.txt', 'at') # 'a' will append to the file 't' is for text
+    # every time we use 'print' it defaults to adding a new line at the end
     print(s, file=fout) # send the print output to our file access object
 
 if __name__ == '__main__':
