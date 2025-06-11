5dee1b16c6f748d8b4286f3aa9c242ae4bbb854f
onionmccabbage <onionmccabbage@live.com>
2025-06-11 16:01:42 +0100

    read, readline and readlines

--- fifo/read_file.py
+++ fifo/read_file.py
@@ -3,8 +3,10 @@
 def readFileData():
     '''Use a file access object to read ext back from a persistent local file'''
     fin = open('my_log.txt', 'r') # 'r' will read from the file. Default is text
-    with fin:
-        retrieved = fin.read() # read the entire file
+    with fin: # the 'with' operation will close the file when we are done
+        # retrieved = fin.read() # read the entire file
+        # retrieved = fin.readline() # read just one line from the file
+        retrieved = fin.readlines() # read all lines into a list of strings
     return retrieved
 
 if __name__ == '__main__':
