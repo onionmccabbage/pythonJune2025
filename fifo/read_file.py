19ef173c00c4255e60bde6eec47e42ed6dbb7b58
onionmccabbage <onionmccabbage@live.com>
2025-06-11 15:57:36 +0100

    using a file access object to read text from a file

---
+++ fifo/read_file.py
@@ -0,0 +1,11 @@
+# we may read a text file using a file access object
+
+def readFileData():
+    '''Use a file access object to read ext back from a persistent local file'''
+    fin = open('my_log.txt', 'r') # 'r' will read from the file. Default is text
+    with fin:
+        retrieved = fin.read() # read the entire file
+
+if __name__ == '__main__':
+    r = readFileData()
+    print(r, type(r))
