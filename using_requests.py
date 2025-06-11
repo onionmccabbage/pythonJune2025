23991fd45b8361f5324feedb26b6b1f4e444377e
onionmccabbage <onionmccabbage@live.com>
2025-06-11 10:58:59 +0100

    using API requests

--- using_requests.py
+++ using_requests.py
@@ -2,12 +2,12 @@
 # NB we may need to pip install requests
 import requests #  this can retrieve JSON, xml, text, html.....
 
-def getData():
+def getData(n): # whichever value is passed in will be 'n'
     '''use the requests library to make a call to an API end-point
     When the JSON data is retrieved, show it in the console'''
     # first make a request to an API end-point
-    response = requests.get('https://jsonplaceholder.typicode.com/users')
-    # next we grab the data from teh response
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{n}')
+    # next we grab the data from the response
     data = response.json() # this will extract any JSON from the response object
     return data # NB the requests library will automatically convert the JSON into a Python structure
 
@@ -16,5 +16,5 @@ def askUser():
 
 if __name__ == '__main__':
     # we may exercise our code here
-    d = getData()
+    d = getData(1) # here we pass 1 as an argument
     print(d, type(d))
