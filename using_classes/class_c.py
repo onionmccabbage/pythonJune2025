4ba35adc9c05c4dee2c4cd27eb2e2f0a0bdfac9f
onionmccabbage <onionmccabbage@live.com>
2025-06-12 13:02:47 +0100

    text for review 3

---
+++ using_classes/class_c.py
@@ -0,0 +1,8 @@
+# We need a 'Weather' class
+# this will have properties
+# Each property must be validated
+# - location (city, country) or whatever you choose (non-empty strings)
+# - temperature numeric float value between -70 and +90
+# - description non-empty string
+
+# Optional - override __str__
