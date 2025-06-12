d3662b43d15bfa39530428592f807800c1cfb6e5
onionmccabbage <onionmccabbage@live.com>
2025-06-12 10:59:15 +0100

    class inheritance

--- using_classes/class_b.py
+++ using_classes/class_b.py
@@ -12,7 +12,7 @@ class Photo(): # we may choose to include ()
         return self.__id # __id is the mangled name
     @id.setter
     def id(self, new_id):
-        if type(new_id)==int and int >=0:
+        if type(new_id)==int and new_id >=0:
             self.__id = new_id
         else:
             raise TypeError('ID must be a positive integer')
@@ -25,3 +25,16 @@ class Photo(): # we may choose to include ()
             self.__title= new_title
         else:
             raise TypeError('title must be a non-empty string')
+        
+
+# class inheritance. If we wish we can use one class to build another class
+class Snapshot(Photo):
+    '''Snapshot inherits everything fro mthe Photo class
+    We can the nadd our own additional features
+    - flash indicator True or False'''
+        
+if __name__ == '__main__':
+    p1 = Photo(2, 'view of Genoa')
+    # p2 = Photo(-2, 'view of Galway Bay') # should throw a type error
+    s1 = Snapshot(4, 'Beutiful Hemel')
+    print(f'{s1.title} has id {s1.id}')
